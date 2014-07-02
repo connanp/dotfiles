9c7472dcfcc3cf1675e83fe70a961aae503fa25f
Connan Pearson <connanp@gmail.com>
2014-07-02 00:00:09 -0700

    Tmux config and bin scripts

---
+++ bin/tmux.start.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+##################
+# TMUX auto-start
+
+_TMUX="tmux"
+# abort if we're already inside a TMUX session
+[ "$TMUX" = "" ] || exit 0
+
+# startup a "default" session if none currently exists
+$_TMUX has-session -t _default 2>/dev/null || $_TMUX new-session -s _default -d
+
+# present menu for user to choose which workspace to open
+PS3="Please choose your session: "
+options=($($_TMUX list-sessions -F "#S") "NEW SESSION" "ZSH")
+echo "Available sessions"
+echo "------------------"
+echo " "
+select opt in "${options[@]}"
+do
+  case $opt in
+    "NEW SESSION")
+      read -p "Enter new session name: " SESSION_NAME
+      $_TMUX -2 new -s "$SESSION_NAME"
+      break
+      ;;
+    "ZSH")
+      zsh --login
+      break;;
+    *)
+      $_TMUX -2 attach-session -t $opt
+      break
+      ;;
+  esac
+done
